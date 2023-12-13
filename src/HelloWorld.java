af35111a54be7e82e40190121c94c125f25de121
Cody R Fishback <cfishback1@gmail.com>
2023-12-13 08:55:35 -0600

    worked on the exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,24 @@ public class HelloWorld {
     }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int myFavoriteNumber = 22;
+        String MyString = "Hello From Cody";
+       /* String myString = 3.145159;
+        float myNumber = 3.14;*/
+        /*int x = 5;*/
+
+        /*System.out.println(x);*/
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+       /* int x = 4;
+        x += 5;*/
+
+        int x = 3;
+        int y = 4;
+        y = y * x;
+
+
     }
 }
